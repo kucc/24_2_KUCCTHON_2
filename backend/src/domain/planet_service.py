bc76bb6cc55e8f9160b71ed14a26fe3b34cf454e
임유나 <youna0104@naver.com>
2024-11-02 03:28:15 +0900

    feat: 방명록 조회

--- backend/src/domain/planet_service.py
+++ backend/src/domain/planet_service.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException, status
-from models import Planet, User
+from models import Comments, Planet, User
+from schema.comments_schema import Comment, ResGetComments
 from schema.planet_schema import ResGetPlanet
 from sqlalchemy import select
 from sqlalchemy.exc import NoResultFound
@@ -60,4 +61,43 @@ async def service_read_planet(user_id: int, db: Session):
     like_count=planet.like_count
   )
 
+  return response
+
+async def service_read_comments(user_id: int, db: Session):
+  stmt = select(Planet).where(Planet.user_id==user_id)
+  try:
+    planet = db.execute(stmt).scalar_one()
+  except NoResultFound:
+      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                          detail="Not found") from NoResultFound
+  stmt = select(Comments).where(Comments.planet_id == planet.id)
+  try:
+    my_comments = db.execute(stmt).order_by(Comments.created_at).scalars().all()
+  except NoResultFound:
+      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                          detail=f"Comments not found for planet {planet.planet_name}") from NoResultFound
+  comments = []
+  for my_comment in my_comments:
+    # User 조회
+    stmt = select(User).where(User.id == my_comment.user_id)
+    try:
+      user = db.execute(stmt).scalar_one()  # 단일 사용자 가져오기
+    except NoResultFound:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="User not found for comment") from NoResultFound
+
+    # 응답 구성
+    comments.append(
+        Comment(
+            user_name=user.user_name,
+            content=my_comment.content,
+            created_at=my_comment.created_at,
+        )
+    )
+  response = ResGetComments(
+    planet_name=planet.planet_name,
+    user_name=user.user_name,
+    comments=comments,
+    count=len(comments),
+  )
   return response
--- backend/src/route/planet_route.py
+++ backend/src/route/planet_route.py
@@ -1,7 +1,8 @@
 from config import Settings
 from dependencies import get_db
-from domain.planet_service import service_read_all_planet, service_read_planet
+from domain.planet_service import service_read_all_planet, service_read_comments, service_read_planet
 from fastapi import APIRouter, Depends, status
+from schema.comments_schema import ResGetComments
 from schema.planet_schema import ResGetAllPlanet, ResGetPlanet
 from sqlalchemy.orm import Session
 
@@ -36,4 +37,17 @@ async def get_planet(
     db: Session = Depends(get_db)
 ):
     result = await service_read_planet(user_id, db)
+    return result
+
+@router.get(
+    "/{user_id}/comments",
+    summary="행성 방명록 조회",
+    response_model=ResGetComments,
+    status_code=status.HTTP_200_OK,
+)
+async def get_comments(
+    user_id: int,
+    db: Session = Depends(get_db)
+):
+    result = await service_read_comments(user_id, db)
     return result
