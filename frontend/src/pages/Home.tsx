6e404e786be92d29152c6cc8c68d04b5d776d334
MJ <alswns8495@naver.com>
2024-11-01 22:55:36 +0900

    feat:홈 화면 백그라운드 적용

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom/client";
 import styles from "./Home.module.css";
 
 const Home: React.FC = () => {
-  return <div className={styles.background}>hi</div>;
+  return (
+    <div className={styles.background}>
+      <h1>hihihi</h1>
+    </div>
+  );
 };
 
 export default Home;
