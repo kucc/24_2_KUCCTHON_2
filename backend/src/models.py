00d03cb1540986c5e1f3288ff83e20f0631e31d8
sounmu <hyginus011111@gmail.com>
2024-11-01 21:58:35 +0900

    feat: models.py 완성

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import TIMESTAMP, Boolean, Column, Date, DateTime, ForeignKey, Integer, String, Text
+from sqlalchemy import TIMESTAMP, Column, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy.sql import func
 
@@ -13,7 +13,8 @@ class User(Base):
     user_name = Column(String(20), nullable=False)
 
     planet = relationship("Planet", back_populates="user")
-
+    comments = relationship("Comments", back_populates="user")
+    likes = relationship("Likes", back_populates="user")
 
 class Planet(Base):
     __tablename__ = "planet"
@@ -21,31 +22,36 @@ class Planet(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
     planet_name = Column(String(20), nullable=False)
-    head = Column(Integer, nullable=True)
-    pet = Column(Integer, nullable=True)
-    eye = Column(Integer)
-    tool = Column(Integer, nullable=True)
+    head = Column(Integer, nullable=False)
+    pet = Column(Integer, nullable=False)
+    eye = Column(Integer, nullable=False)
+    tool = Column(Integer, nullable=False)
     like_count = Column(Integer, default=0)
     
     user = relationship("User", back_populates="planet")
-    comments = relationship("Comments", back_populates="comments")
-
+    comments = relationship("Comments", back_populates="planet")
+    likes = relationship("Likes", back_populates="planet")
 
 class Comments(Base):
     __tablename__ = "comments"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    planet_id = Column(Integer, ForeignKey("planet.id"), nullable=False)
     user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    planet_id = Column(Integer, ForeignKey("planet.id"), nullable=False)
     content = Column(Text, nullable=False)
     created_at = Column(TIMESTAMP, nullable=False, default=func.current_timestamp())
 
+    user = relationship("User", back_populates="comments")
     planet = relationship("Planet", back_populates="comments")
-    user = relationship("User", back_populates="planet")
 
 
 class Likes(Base):
     __tablename__ = "likes"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    planet_id = Column(Integer, ForeignKey("planet.id"), nullable=False)
+    liked_at = Column(TIMESTAMP, nullable=False, default=func.current_timestamp())
 
+    user = relationship("User", back_populates="likes")
+    planet = relationship("Planet", back_populates="likes")
